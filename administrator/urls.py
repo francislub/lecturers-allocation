fcf49fe48242bec4987dbfd162739851ee1342e4
francislub <lubanjwafrancis@gmail.com>
2024-03-29 21:08:40 -0700

    more today

--- administrator/urls.py
+++ administrator/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.dashboard, name="adminDashboard"),
     path('reqst/', views.reqStatus, name="reqst"),
     path('repst/', views.reportStatus, name="repst"),
+    path('requistition_phase1/', views.requistition_phase1, name="requistition_phase1"),
     path('lecturer/', views.lecturerDashboard, name="lecturer"),
     
 ]
