885812a6aa94748096ff73fb6ce7942eb3c8efd4
TroyMoses <mugabimoses07@gmail.com>
2024-04-17 09:37:15 +0600

    urls

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.account_login, name="account_login"),
-    path('register/', views.account_register, name="account_register"),
+    # path('', views.account_login, name="account_login"),
+    path('', views.login, name="login"),
+    # path('register/', views.account_register, name="account_register"),
+    path('register/', views.register_user, name='register'),
     path('dep/', views.create_department, name="create_department"),
     path('logout/', views.account_logout, name="account_logout"),
     path('lock/', views.lock, name='register'),
