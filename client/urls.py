064a026a9663d68756dd1a6bf5c6901133599fe9
TroyMoses <mugabimoses07@gmail.com>
2024-04-17 11:20:12 +0600

    urls

--- client/urls.py
+++ client/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
-    path('lock/', views.lock, name='register'),
+    # path('register/', views.register, name='register'),
+    # path('lock/', views.lock, name='lock'),
     path('dep/', views.dep, name='dep'),
     path('choices_form/', views.choices_form, name='choices_form'),
     path('',views.staffDashboard,name='staffDashboard'),
